a8dab7b6db57c49465ae9321cd031f0fdce0af73
Guillaume Corlouer <35979217+gcorlouer@users.noreply.github.com>
2019-02-21 23:30:50 +0000

    Add files via upload

---
+++ tsa_code.py
@@ -0,0 +1,34 @@
+import numpy as np
+import random 
+import statsmodels.api as sm
+from statsmodels.tsa.arima_process import arma_generate_sample
+import matplotlib.pyplot as plt 
+#1D Random walk
+N=500 #number of observations
+#Initialise
+X=np.zeros((1,N), float)
+epsilon=np.random.normal(loc=0.0, scale=1, size=(1,N))
+X[0,0]=epsilon[0,0]
+#Generate Random Walk
+for i in range(N-1):
+    X[0,i+1]=X[0,i]+epsilon[0,i]
+plt.plot(X[0,:])
+#ARMA(2,2)
+arparams = np.array([.75, -.25])
+maparams = np.array([.65, .35])
+arparams = np.r_[1, -arparams]#step is zero is unweighted
+maparams = np.r_[1, maparams]#step is zero is unweighted
+y = arma_generate_sample(arparams, maparams, 500)#mean zero variance 1
+plt.plot(y)
+#AR(1)
+#Stable
+X=np.zeros((1,N), float)
+epsilon=np.random.normal(loc=0.0, scale=1, size=(1,N))
+X[0,0]=epsilon[0,0]
+for i in range(N-1):
+    X[0,i+1]=0.5*X[0,i]+epsilon[0,i]
+plt.plot(X[0,:])
+#Unstable
+for i in range(N-1):
+    X[0,i+1]=2*X[0,i]+epsilon[0,i]
+plt.plot(X[0,:])
